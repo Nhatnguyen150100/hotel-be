eaf77458e052a8a515ca178e64553cc777ada0bb
nhatnguyen150100 <nhatnguyen150100@gmail.com>
2024-12-03 14:23:33 +0700

    fix: seed

--- src/seeders/20241004102838-User.js
+++ src/seeders/20241004102838-User.js
@@ -3,39 +3,22 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-     */
-    await queryInterface.bulkInsert(
-      "Users",
-      [
-        {
-          id: "d511aeab-f46d-248c-a29d-55ad1855651a",
-          email: "admin@gmail.com",
-          role: "ADMIN",
-          password:
-            "$2b$10$DChhTLTf0XAHg67cI45CDeFBDLOze1zkicc3Bf2kBk7SUpfBnK8iC", // plain = Admin@123
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-      ],
-      {}
-    );
+    const userData = {
+      id: "d511aeab-f46d-248c-a29d-55ad1855651a",
+      email: "admin@gmail.com",
+      role: "ADMIN",
+      password:
+        "$2b$10$DChhTLTf0XAHg67cI45CDeFBDLOze1zkicc3Bf2kBk7SUpfBnK8iC", // plain = Admin@123
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    await queryInterface.bulkInsert('Users', [userData], {
+      updateOnDuplicate: ["email", "role", "password", "updatedAt"]
+    });
   },
 
   async down(queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
     return queryInterface.bulkDelete('Users', null, {});
   },
 };
